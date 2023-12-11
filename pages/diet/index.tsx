75f4ee9990bdb1631656346e6453b2fe16c03a29
jongho123741 <136421728+jongho123741@users.noreply.github.com>
2023-12-11 17:19:03 +0900

    diet page default layout done

--- pages/diet/index.tsx
+++ pages/diet/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import {
   Card,
   CardHeader,
+  Carousel,
   CardBody,
   CardFooter,
   Typography,
@@ -16,6 +17,7 @@ interface monthDateObj {
 
 function index() {
   const [monthDate, setMonthDate] = useState<monthDateObj>();
+  const [selectedDate, setSelectedDate] = useState<Number>();
 
   useEffect(() => {
     const date = new Date();
@@ -23,14 +25,15 @@ function index() {
     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     const dateObj = { first: firstDay.getDate(), last: lastDay.getDate() };
     setMonthDate(dateObj);
+    setSelectedDate(date.getDate());
   }, []);
 
   //   var first = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week
   //   var last = first + 6; // last day is the first day + 6
   return (
     monthDate?.last && (
-      <div>
-        <div className="flex flex-row overflow-auto">
+      <section>
+        <article className="flex flex-row overflow-auto">
           {Array.from({ length: monthDate?.last }, (_, index) => {
             return (
               <Card className="mt-6 mx-3 w-96">
@@ -59,9 +62,38 @@ function index() {
               </Card>
             );
           })}
-        </div>
-        <h1>here is the main diary?</h1>
-      </div>
+        </article>
+        <article className="w-full border-solid border-t-light-blue-600 rounded-md">
+          <hgroup>
+            <h1>참가자</h1>
+            <h3>tim</h3>
+            <h3>anyone</h3>
+          </hgroup>
+        </article>
+        <article>
+          <div className="w-full border-solid border-t-black rounded-md">
+            <Button className="flex items-center gap-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                />
+              </svg>
+              등록하기
+            </Button>
+            <div>main content here</div>
+          </div>
+          {/* <aside>aside posting and editing?</aside> */}
+        </article>
+      </section>
     )
   );
 }
