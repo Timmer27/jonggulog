9029aa77edae182995d0ed8dd6588fb73ddabd53
Timmer <whdghtpgml@gmail.com>
2023-12-17 18:16:30 +0900

    일상 테이블 헤더 및 기본 레이아웃 구성

--- pages/daily/index.tsx
+++ pages/daily/index.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
 
-type Props = {}
-
-export default function post({}: Props) {
+export default function Daily({}) {
+  const [activeTab, setActiveTab] = React.useState<string>("html");
+  const data = [
+    {
+      label: "최신",
+      value: "latest",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "업데이트",
+      value: "update",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+  ];
   return (
-    <div className='mt-[7rem] mb-10 w-[68%] m-auto'>dayil</div>
-  )
+    <Tabs value={activeTab} className="mt-10 ml-7">
+      <TabsHeader
+        className="w-56"
+        indicatorProps={{
+          className:
+            "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+        }}
+      >
+        {data.map(({ label, value }) => (
+          <Tab
+            key={value}
+            value={value}
+            onClick={() => setActiveTab(value)}
+            className={activeTab === value ? "text-gray-900" : ""}
+          >
+            {label}
+          </Tab>
+        ))}
+      </TabsHeader>
+      <TabsBody>
+        {data.map(({ value, desc }) => (
+          <TabPanel key={value} value={value}>
+            {desc}
+          </TabPanel>
+        ))}
+      </TabsBody>
+    </Tabs>
+  );
 }
---
+++ pages/programs/naverProgram.tsx
@@ -0,0 +1,25 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+} from "@material-tailwind/react";
+
+export function BookingCard() {
+  return (
+    // shadow-[0px_0px_9px_-1px_#b3b3b347]
+    <div className="w-full  p-4 border-[#d0d0d0] rounded-xl">
+      <Typography variant="h5" color="gray">
+        순서 1
+      </Typography>
+      <Typography color="gray">
+        Enter a freshly updated and thoughtfully furnished peaceful home
+        surrounded by ancient trees, stone walls, and open meadows.
+      </Typography>
+    </div>
+  );
+}
