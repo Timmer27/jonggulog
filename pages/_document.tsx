01ee538add8dacc39e7a3872f8f05f7c00641462
Timmer <95391871+Timmer27@users.noreply.github.com>
2024-01-07 22:59:38 +0900

    구글 SEO 재등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,15 @@ class MyDocument extends Document {
       // html 태그에 언어 설정
       <Html lang="ko">
         {/* <Head> 태그에는 모든 문서에 공통으로 적용할 (charset, 뷰포트 메타태그 등)이 들어갑니다. */}
-        <meta name="google-site-verification" content="Bd1SUoTNWXeUMdGKzD1_FYj0KmA0tSwJZcFoXozsMHE" />
-        <meta name="naver-site-verification" content="4c7049efad60cdc78b96269784680cad9b4d0ce2" />
+        {/* google-site-verification=Bd1SUoTNWXeUMdGKzD1_FYj0KmA0tSwJZcFoXozsMHE */}
+        <meta
+          name="google-site-verification"
+          content="pD-tS2jovinCyQSbO-JntskUC-t7JEdT-207MbaK29Q"
+        />
+        <meta
+          name="naver-site-verification"
+          content="4c7049efad60cdc78b96269784680cad9b4d0ce2"
+        />
         <Head />
         <body>
           <Main />
